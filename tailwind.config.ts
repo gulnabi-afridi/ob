45eea9b37bd177e97acbe29f2f357dbaf5e8eee5
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-02-19 12:13:24 +0500

    tailwind config edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,50 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        termina: ['Termina Test', 'sans-serif'],
+      },
+      screens: {
+        xs: '325px',
+        xsm: '370px',
+        sm: '480px',
+        csm: '540px',
+        md: '768px',
+        cmd: '914px',
+        lg: '976px',
+        clg: '1182px',
+        xl: '1280px',
+        '1xl': '1400px',
+      },
+      colors: {
+        black: {
+          1: '#000000',
+          2: '##090B0B',
+          3: '#121616',
+        },
+        purple: {
+          1: '#6C5DD3',
+        },
+        white: {
+          1: '#FFF',
+          2: '#DDD',
+          3: '#D9D9D9',
+        },
+        green: {
+          1: '#50C878',
+        },
       },
     },
   },
