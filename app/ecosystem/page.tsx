c66d1ff1dbd296cee66a89afcef8c6270b6e76ee
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-02-25 13:45:06 +0500

    ecosystem optimized

--- app/ecosystem/page.tsx
+++ app/ecosystem/page.tsx
@@ -5,6 +5,7 @@ import Wrapper from '../component/shared/ComponentWrapper/Wrapper';
 import Image from 'next/image';
 import * as Icons from '../svg/Icons';
 import { Fade, Slide } from 'react-awesome-reveal';
+import Link from 'next/link';
 
 const page = () => {
   return (
@@ -22,56 +23,78 @@ const page = () => {
             </div>
           </Fade>
           <div className='w-full grid md:gap-0 gap-8 sm:gap-16 grid-cols-1 md:grid-cols-2 '>
-            <Slide direction='left' duration={800} delay={200}>
+            <Slide
+              direction='left'
+              duration={800}
+              delay={200}
+              className='w-full z-30'
+            >
               {/* 1st col */}
-              <div className='w-full flex justify-center items-center'>
-                <div className='w-full max-w-[300px] flex flex-col gap-8 sm:gap-16 md:gap-16 justify-center items-center'>
-                  <div className='w-full flex flex-col justify-center items-center gap-3'>
-                    <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
-                      TRADE
-                    </p>
-                    <div className='w-[160px] sm:w-[250px] h-[26px] sm:h-[50px] relative'>
-                      <Image
-                        src='/assets/prism.svg'
-                        fill
-                        alt=''
-                        className='object-fill'
-                      />
-                    </div>
+              <div className='w-full grid grid-cols-1 h-full gap-8 sm:gap-20 justify-center items-center'>
+                {/* trade link --->  */}
+                <Link
+                  href='#'
+                  className='w-full cursor-pointer hover:opacity-60 flex flex-col justify-center items-center gap-3'
+                >
+                  <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
+                    TRADE
+                  </p>
+                  <div className='w-[160px] sm:w-[250px] h-[25px] sm:h-[50px] relative'>
+                    <Image
+                      src='/assets/prism.svg'
+                      fill
+                      alt=''
+                      className='object-fill'
+                    />
                   </div>
-                  <div className='w-full flex flex-col justify-center items-center gap-0'>
-                    <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
-                      SWAP
-                    </p>
-                    <Icons.jupiter className='w-[160px] sm:w-[256px] h-[50px] sm:h-[78px]' />
-                  </div>
-                </div>
+                </Link>
+                {/* swap link --->  */}
+                <Link
+                  href='#'
+                  className='w-full hover:opacity-60 flex flex-col justify-center items-center gap-0'
+                >
+                  <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
+                    SWAP
+                  </p>
+                  <Icons.jupiter className='w-[160px] sm:w-[256px] h-[50px] sm:h-[78px]' />
+                </Link>
               </div>
             </Slide>
-            <Slide direction='right' duration={800} delay={200}>
+            <Slide
+              direction='right'
+              duration={800}
+              delay={200}
+              className='z-30'
+            >
               {/* 2st col */}
-              <div className='w-full flex justify-center items-center'>
-                <div className='w-full max-w-[300px] flex flex-col gap-8 sm:gap-16 md:gap-16 justify-center items-center'>
-                  <div className='w-full flex flex-col justify-center items-center gap-3'>
-                    <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
-                      ANALYTICS
-                    </p>
-                    <div className='w-[160px] sm:w-[250px] h-[39px] sm:h-[50px] relative'>
-                      <Image
-                        src='/assets/arcana.svg'
-                        fill
-                        alt=''
-                        className='object-fill'
-                      />
-                    </div>
+              <div className='w-full grid h-full grid-cols-1 gap-8 sm:gap-20 justify-center items-center'>
+                {/* arcana link --->  */}
+                <Link
+                  href='#'
+                  className='w-full hover:opacity-70 flex flex-col justify-center items-center gap-3'
+                >
+                  <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
+                    ANALYTICS
+                  </p>
+                  <div className='w-[180px] sm:w-[300px] h-[45px] sm:h-[80px] relative'>
+                    <Image
+                      src='/assets/arcana.svg'
+                      fill
+                      alt=''
+                      className='object-fill'
+                    />
                   </div>
-                  <div className='w-full flex flex-col justify-center items-center gap-0'>
-                    <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
-                      CREATE
-                    </p>
-                    <Icons.grapple className='w-[160px] sm:w-[256px] h-[44px] sm:h-[78px]' />
-                  </div>
-                </div>
+                </Link>
+                {/* grapple link -->  */}
+                <Link
+                  href='#'
+                  className='w-full hover:opacity-70 flex flex-col justify-center items-center gap-0'
+                >
+                  <p className='text-[12px] sm:text-[18px] lg:text-left text-center font-normal textColor'>
+                    CREATE
+                  </p>
+                  <Icons.grapple className='w-[200px] sm:w-[306px] h-[50px] sm:h-[88px]' />
+                </Link>
               </div>
             </Slide>
           </div>
